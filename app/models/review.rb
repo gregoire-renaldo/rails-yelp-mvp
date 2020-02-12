90a5f05db684bbe0db3803e50de7cdf19564d06e
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-02-12 16:40:19 +0100

    restaurants

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
-  validates :name, uniqueness: true, presence: true
   validates :content, :rating, presence: true
-  validates :rating, inclusion: { in: [1,2,3,4,5] }
+  validates :rating, numericality: { only_integer: true }
+  validates :content, length: { minimum: 1 }
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
 end
 
 # A review must have a parent restaurant.
